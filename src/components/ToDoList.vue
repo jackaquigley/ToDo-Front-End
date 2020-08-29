b2cb1d049ba1f090b2c67048146a44355f7513a8
jackaquigley <jackaquigley@gmail.com>
2020-08-29 21:28:08 +0100

    created ToDoList component and created div with name and completed status for each todo in the database

---
+++ src/components/ToDoList.vue
@@ -0,0 +1,18 @@
+<template lang="html">
+  <div class="toDoItem">
+    <p>{{todo.name}}</p>
+    <p>{{todo.completed}}</p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'todo-list',
+  props: ['todo']
+  }
+</script>
+
+<style lang="css">
+
+</style>
--- src/views/ToDo.vue
+++ src/views/ToDo.vue
@@ -1,22 +1,26 @@
 <template lang="html">
-  <div class="component-wrapper">
-    <p>ToDo.vue</p>
+  <div class="toDoList">
+    <todo-list v-for="(todo, index) in todos" :key="index" :todo="todo" />
   </div>
 </template>
 
 <script>
 
+import ToDoList from '@/components/ToDoList.vue'
 import ToDoService from '@/services/ToDoService.js'
 
-  export default {
-    data() {
-      return {
-        todos: []
-      };
+export default {
+  data() {
+    return {
+      todos: []
+    };
+  },
+  mounted() {
+    ToDoService.getToDos()
+    .then(todos => this.todos = todos);
     },
-    mounted() {
-      ToDoService.getToDos()
-      .then(todos => this.todos = todos);
+  components: {
+    'todo-list': ToDoList
     }
   }
 </script>
